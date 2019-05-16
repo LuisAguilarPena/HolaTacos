e69cdf3b5f20237915d0999426d75e5199efbf46
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-05-16 12:23:33 -0500

    Outputting Burger Ingredients Dynamically

--- src/components/Taco/Taco.js
+++ src/components/Taco/Taco.js
@@ -3,12 +3,18 @@ import classes from './Taco.module.css';
 import TacoIngredient from './TacoIngredients/TacoIngredient';
 
 const taco = props => {
+  // [tortilla, pastor, suadero...]
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      // an array with empty arrays or arrays with undefined for every ingredient
+      return [...Array(props.ingredients[igKey])]
+        // underscore is a blank but the index is required  
+        .map((_, i) => <TacoIngredient key={igKey+i} type={igKey}/>)
+    });
   return (
     <div className={classes.Taco}>
       <TacoIngredient type="tortilla"/>
-      <TacoIngredient type="pollo"/>
-      <TacoIngredient type="cilantro"/>
-      <TacoIngredient type="onion"/>
+      {transformedIngredients}
     </div>
   );
 };
--- src/containers/TacoBuilder/TacoBuilder.js
+++ src/containers/TacoBuilder/TacoBuilder.js
@@ -3,10 +3,23 @@ import Aux from '../../hoc/Aux';
 import Taco from '../../components/Taco/Taco';
 
 class TacoBuilder extends Component {
+  state = {
+    ingredients: {
+      tortilla: 1,
+      pastor: 0,
+      suadero: 0,
+      bistec: 0,
+      longaniza: 1,
+      pollo: 0,
+      cilantro: 1,
+      onion: 1,
+      pina: 0
+    }
+  }
   render () {
     return (
       <Aux>
-        <Taco/>
+        <Taco ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </Aux>
     );
