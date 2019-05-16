65f349a2d3af129a68ddd4624a4a06d278afea54
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-05-15 19:11:53 -0500

    Implementation of the Taco Builder Container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
-
+import TacoBuilder from './containers/TacoBuilder/TacoBuilder';
 // Root component of our application, all other components will be nested here
 class App extends Component{
   render(){
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <TacoBuilder/>
         </Layout>
       </div>
     );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
 // this is my high order component that I'm using to wrap my elements so I don't have to use a div and that way components can be next to each other and I don't clutter the DOM with a bunch of divs
 import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css'
 
 const layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
