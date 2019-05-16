afd959efe0b95a970b105960c733b969746a03ed
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-05-16 10:19:48 -0500

    Added Prop Type Validation for our ingredients

--- src/components/Taco/TacoIngredients/TacoIngredient.js
+++ src/components/Taco/TacoIngredients/TacoIngredient.js
@@ -1,40 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classes from './TacoIngredient.module.css';
 
-const tacoIngredient = props => {
-  let ingredient = null;
-  switch (props.type) {
-    case ('tortilla'):
-      ingredient = <div className={classes.tortilla}></div>;
-      break;
-    case('pastor'):
-      ingredient = <div className={classes.pastor}></div>;
-      break;
-    case('suadero'):
-      ingredient = <div className={classes.suadero}></div>;
-      break;
-    case('bistec'):
-      ingredient = <div className={classes.bistec}></div>;
-      break;
-    case('longaniza'):
-      ingredient = <div className={classes.longaniza}></div>;
-      break;
-    case('pollo'):
-      ingredient = <div className={classes.pollo}></div>;
-      break;
-    case('pina'):
-      ingredient = <div className={classes.pina}></div>;
-      break;
-    case('cilantro'):
-      ingredient = <div className={classes.cilantro}></div>;
-      break;
-    case('onion'):
-      ingredient = <div className={classes.onion}></div>;
-      break;
-    default:
-      ingredient = null;  
+class TacoIngredient extends Component{
+  render(){
+    let ingredient = null;
+    switch (this.props.type) {
+      case ('tortilla'):
+        ingredient = <div className={classes.tortilla}></div>;
+        break;
+      case('pastor'):
+        ingredient = <div className={classes.pastor}></div>;
+        break;
+      case('suadero'):
+        ingredient = <div className={classes.suadero}></div>;
+        break;
+      case('bistec'):
+        ingredient = <div className={classes.bistec}></div>;
+        break;
+      case('longaniza'):
+        ingredient = <div className={classes.longaniza}></div>;
+        break;
+      case('pollo'):
+        ingredient = <div className={classes.pollo}></div>;
+        break;
+      case('pina'):
+        ingredient = <div className={classes.pina}></div>;
+        break;
+      case('cilantro'):
+        ingredient = <div className={classes.cilantro}></div>;
+        break;
+      case('onion'):
+        ingredient = <div className={classes.onion}></div>;
+        break;
+      default:
+        ingredient = null;  
+    }
+    return ingredient;
   }
-  return ingredient;
+}
+TacoIngredient.propTypes={
+  // check that ingredient is a str
+  type: PropTypes.string.isRequired
 };
-
-export default tacoIngredient;
+export default TacoIngredient;
