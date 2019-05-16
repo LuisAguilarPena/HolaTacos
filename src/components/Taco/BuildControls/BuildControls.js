1f1e79216432aae9bce1b9b40219123a359357ad
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-05-16 16:43:56 -0500

    connecting state to build controls, more button works, we add unlimeted amouns of ingredients lets limit to 1, less button not working yet,

--- src/components/Taco/BuildControls/BuildControls.js
+++ src/components/Taco/BuildControls/BuildControls.js
@@ -3,19 +3,23 @@ import classes from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl';
 // an array to conveniently build the controls
 const controls = [
-  {label: 'tortilla', type: 'base'},
-  {label: 'pastor', type: 'meat'},
-  {label: 'suadero', type: 'meat'},
-  {label: 'bistec', type: 'meat'},
-  {label: 'longaniza', type: 'meat'},
-  {label: 'pollo', type: 'meat'},
-  {label: 'cilantro', type: 'veggie'},
-  {label: 'onion', type: 'veggie'},
-  {label: 'pina', type: 'veggie'}
+  {label: 'Tortilla', type: 'tortilla'},
+  {label: 'Pastor', type: 'pastor'},
+  {label: 'Suadero', type: 'suadero'},
+  {label: 'Bistec', type: 'bistec'},
+  {label: 'Longaniza', type: 'longaniza'},
+  {label: 'Pollo', type: 'pollo'},
+  {label: 'Cilantro', type: 'cilantro'},
+  {label: 'Onion', type: 'onion'},
+  {label: 'Pina', type: 'pina'}
 ]
 const buildControls = props => (
   <div className={classes.BuildControls}>
-    {controls.map(ctrl => <BuildControl key={ctrl.label} label={ctrl.label} />)}
+    {controls.map(ctrl => <BuildControl 
+      key={ctrl.label} 
+      label={ctrl.label}
+      added={() => props.ingredientAdded(ctrl.type)} />
+    )}
   </div>
 );
 
--- src/containers/TacoBuilder/TacoBuilder.js
+++ src/containers/TacoBuilder/TacoBuilder.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Taco from '../../components/Taco/Taco';
 import BuildControls from '../../components/Taco/BuildControls/BuildControls';
+
 class TacoBuilder extends Component {
   state = {
     ingredients: {
-      tortilla: 1,
+      tortilla: 0,
       pastor: 0,
       suadero: 0,
       bistec: 0,
@@ -14,13 +15,30 @@ class TacoBuilder extends Component {
       cilantro: 0,
       onion: 0,
       pina: 0
-    }
+    },
+    totalPrice: 20
   }
+
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type]
+    const updatedCount = oldCount +1;
+    // state should be updated in an inmutable way
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type]=updatedCount;
+    this.setState({ingredients: updatedIngredients})
+  }
+
+  removeIngredient = type => {
+
+  }
+
   render () {
     return (
       <Aux>
         <Taco ingredients={this.state.ingredients}/>
-        <BuildControls />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </Aux>
     );
   }
