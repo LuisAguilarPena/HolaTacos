8e4df9eb35b887f936d2c7c428e88bad51f1dcdd
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-05-16 14:45:07 -0500

    calculating the ingredient sum dynamically

--- src/components/Taco/Taco.js
+++ src/components/Taco/Taco.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import classes from './Taco.module.css';
 import TacoIngredient from './TacoIngredients/TacoIngredient';
+import Aux from '../../hoc/Aux';
 
 const taco = props => {
   // [tortilla, pastor, suadero...]
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       // an array with empty arrays or arrays with undefined for every ingredient
       return [...Array(props.ingredients[igKey])]
         // underscore is a blank but the index is required  
         .map((_, i) => <TacoIngredient key={igKey+i} type={igKey}/>)
-    });
+    })
+    // console.log(transformedIngredients);
+    // array of arrays of obj elements, if an ingredient is 1 otherwise is an empty array, each e is an ingredient and it has multiple things inside
+    // reduce to pull out the values of the inner arrays
+    .reduce((prevValue, currValue) => prevValue.concat(currValue));
+    // the [] after the callback seems to not be necessary for reduce, is an initial value to store in an empty array
+    // when we add more ingredients the obj inner elment will be the elment of array
+    // console.log(transformedIngredients); // ingredients in a flat array   
+    if (transformedIngredients.length ===0) {
+      transformedIngredients = <Aux><br/><br/><p>Tripa vacia corazón sin alegría<br/>Agrega tus ingredientes favoritos </p></Aux>
+    }
   return (
     <div className={classes.Taco}>
-      <TacoIngredient type="tortilla"/>
       {transformedIngredients}
     </div>
   );
--- src/containers/TacoBuilder/TacoBuilder.js
+++ src/containers/TacoBuilder/TacoBuilder.js
@@ -5,14 +5,14 @@ import Taco from '../../components/Taco/Taco';
 class TacoBuilder extends Component {
   state = {
     ingredients: {
-      tortilla: 1,
+      tortilla: 0,
       pastor: 0,
       suadero: 0,
       bistec: 0,
-      longaniza: 1,
+      longaniza: 0,
       pollo: 0,
-      cilantro: 1,
-      onion: 1,
+      cilantro: 0,
+      onion: 0,
       pina: 0
     }
   }
