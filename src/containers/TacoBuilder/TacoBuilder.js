ad06c431fc94be0bd7ae744fb8bf8dcbeaac0f21
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-05-17 11:43:51 -0500

    refactor addIngredientHandler

--- src/containers/TacoBuilder/TacoBuilder.js
+++ src/containers/TacoBuilder/TacoBuilder.js
@@ -6,7 +6,7 @@ import BuildControls from '../../components/Taco/BuildControls/BuildControls';
 class TacoBuilder extends Component {
   state = {
     ingredients: {
-      tortilla: 0,
+      tortilla: 1,
       pastor: 0,
       suadero: 0,
       bistec: 0,
@@ -20,17 +20,18 @@ class TacoBuilder extends Component {
   }
 
   addIngredientHandler = type => {
-    const oldCount = this.state.ingredients[type]
-    let updatedCount;
-    if(oldCount===0) {
-      updatedCount = oldCount +1;
+    let ingredientQuantity = this.state.ingredients[type]
+    if(ingredientQuantity === 0) {
+      console.log(`addIngredientHandler added 1 to ${type}`);
+      // state should be updated in an inmutable way
+      const updatedIngredients = {
+        ...this.state.ingredients
+      };
+      updatedIngredients[type]=ingredientQuantity+1;
+      this.setState({ingredients: updatedIngredients})
     }
-    // state should be updated in an inmutable way
-    const updatedIngredients = {
-      ...this.state.ingredients
-    };
-    updatedIngredients[type]=updatedCount;
-    this.setState({ingredients: updatedIngredients})
+    console.log(this.state.ingredients);
+    
   }
 
   removeIngredient = type => {
