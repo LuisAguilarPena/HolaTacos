a5a46449a71693cd4c2b336b38cf2deb9911bdae
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-05-28 15:56:20 -0500

    Added backdrop and clicked functionality to get out of sidedrawer and backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 // this is my high order component that I'm using to wrap my elements so I don't have to use a div and that way components can be next to each other and I don't clutter the DOM with a bunch of divs
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = props => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
+  
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+  
+  render () {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+}  
 
-export default layout;
+export default Layout;
