e3b837808d7298c9f828aba4cd6eb44e2915a04e
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-05-28 15:15:08 -0500

    added sidedrawer and fixed logo size to be way smaller for sidedrawer

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import HolaTacosLogo from '../../assets/images/Logo.png';
 import classes from './Logo.module.css';
 
 const logo = props => (
-  <div className={classes.Logo}>
+  <div className={classes.Logo} style={{height: props.height}}>
     <img src={HolaTacosLogo} alt="HolaTacos!"/>      
   </div>
 );
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,10 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const Toolbar = props => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
-    <Logo />
+    {/*<Logo height="80%" />*/}
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
     <nav>
       <NavigationItems />
     </nav>
