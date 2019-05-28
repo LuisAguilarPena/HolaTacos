0f89af9956618481844566777740005f55ef9aee
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-05-28 00:16:44 -0500

    Added images for nav items

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
+import contact from '../../../assets/images/contact.png';
+import help from '../../../assets/images/ayuda.png';
+import checkout from '../../../assets/images/checkout.png';
+import avatar from '../../../assets/images/avatar.png';
+import taco from '../../../assets/images/taco.png';
 
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/" active>Taco</NavigationItem>
-    <NavigationItem link="/" >Usuario</NavigationItem>
-    <NavigationItem link="/" >Carrito</NavigationItem>
-    <NavigationItem link="/" >Ayuda</NavigationItem>
-    <NavigationItem link="/" >Contacto</NavigationItem>
+    <NavigationItem link="/" active><img src={taco} alt="avatar"/></NavigationItem>
+    <NavigationItem link="/" ><img src={avatar} alt="avatar"/></NavigationItem>
+    <NavigationItem link="/" ><img src={checkout} alt="Checkout"/></NavigationItem>
+    <NavigationItem link="/" ><img src={help} alt="Ayuda"/></NavigationItem>
+    <NavigationItem link="/" ><img src={contact} alt="Contact"/></NavigationItem>
   </ul>
 )
 
