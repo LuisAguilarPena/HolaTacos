dcaf10948983b77579123c4fc18050d9495fcc36
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-05-29 22:58:39 -0500

    changed Ordersummary to a class comp and created a lifecycle method to check when OrderSum was updating

--- src/components/Taco/OrderSummary/OrderSummary.js
+++ src/components/Taco/OrderSummary/OrderSummary.js
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { Component} from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const  orderSummary = props => {
-  const chosenIngredients = (object) => {
-    let arr = [];
-    let keys = Object.keys(object);
-    let values = Object.values(object);
-    for (let i = 0; i < values.length; i++) {
-      if(values[i]>0) arr.push(keys[i]);
-    }
-    return arr;
+class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log('[OrderSummary] Didupdate');
+    
   }
-  const ingredientSummary = chosenIngredients(props.ingredients) // here I will have an array of lonly the ingredients that are chosen
+
+  render(){
+    const chosenIngredients = (object) => {
+      let arr = [];
+      let keys = Object.keys(object);
+      let values = Object.values(object);
+      for (let i = 0; i < values.length; i++) {
+        if(values[i]>0) arr.push(keys[i]);
+      }
+      return arr;
+    }
+    const ingredientSummary = chosenIngredients(this.props.ingredients) // here I will have an array of lonly the ingredients that are chosen
     .map(igKey => <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span></li>)
-  return (
-    <Aux>
-      <h3>Resumen de orden:</h3>
-      <p><strong>{props.quantity}</strong> tacos con los siguientes ingredientes:</p>
-      <ul>
-        {ingredientSummary}
-      </ul> 
-      <p>Han sido agregados a tu carrito.</p>
-      <p>Total a pagar <strong>{props.quantity*props.price}</strong></p>
-      <p>¿Continuar al carrito?</p>
-      <Button btnType="Success" clicked={props.modalContinue}>Continuar</Button>
-      <Button btnType="Danger" clicked={props.modalCancel}>Cancelar</Button>
-    </Aux>
-  )
+    return (
+      <Aux>
+        <h3>Resumen de orden:</h3>
+        <p><strong>{this.props.quantity}</strong> tacos con los siguientes ingredientes:</p>
+        <ul>
+          {ingredientSummary}
+        </ul> 
+        <p>Han sido agregados a tu carrito.</p>
+        <p>Total a pagar <strong>{this.props.quantity*this.props.price}</strong></p>
+        <p>¿Continuar al carrito?</p>
+        <Button btnType="Success" clicked={this.props.modalContinue}>Continuar</Button>
+        <Button btnType="Danger" clicked={this.props.modalCancel}>Cancelar</Button>
+      </Aux>
+    )}
 };
 
-export default orderSummary;
+export default OrderSummary;
