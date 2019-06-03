93d90291a668f5625927b8ccd813c367b0de7057
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-06-03 16:26:23 -0500

    Set up routing and routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
+import { Route, Switch } from  'react-router-dom';
 import TacoBuilder from './containers/TacoBuilder/TacoBuilder';
 import Checkout from './containers/Checkout/Checkout';
 // Root component of our application, all other components will be nested here
@@ -8,8 +9,10 @@ class App extends Component{
     return (
       <div>
         <Layout>
-          <TacoBuilder/>
-          <Checkout/>
+          <Switch>
+            <Route  path="/checkout" component={Checkout}/>
+            <Route  path="/" exact component={TacoBuilder}/>
+          </Switch>
         </Layout>
       </div>
     );
