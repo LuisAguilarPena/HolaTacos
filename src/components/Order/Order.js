4693f9908f20a275d53db3f533f880e52159c779
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-06-05 18:10:52 -0500

    final styling of the order page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -15,19 +15,13 @@ const order = props => {
   const ingredientOutput = ingredients.map(ig => {
     if(ig.amount>0){
     return <span 
-      style={{
-        textTransform: 'capitalize',
-        display: 'inline-block',
-        margin: '0 8px',
-        border: '4px solid black',
-        padding: '10px'
-        }}
-      key={ig.name}>{ig.name.slice(1)} ({ig.amount})</span>;
+      className={classes.Span}
+      key={ig.name}>{ig.name.slice(1)} {/*({ig.amount})*/}</span>;
   }else {return null}})
   return (
     <div className={classes.Order}>
-      <p>Ingredients: {ingredientOutput}</p>
-      <p>Price: <strong>$ {props.price}</strong></p>    
+      <p><strong>Ingredientes:</strong> {ingredientOutput}</p>
+      <p><strong>{props.price/20} Tacos = </strong><strong>${props.price}</strong></p>    
     </div>
   );
 }
