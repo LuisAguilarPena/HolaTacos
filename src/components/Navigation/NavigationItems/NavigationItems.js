66c3fa6f55eea4e5ac25a9e70282d8121b00928c
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-06-07 12:33:30 -0500

    tooltips  of nav items are responsive

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -9,11 +9,11 @@ import taco from '../../../assets/images/taco.png';
 
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/" ><img src={taco} alt="build"/></NavigationItem>
-    <NavigationItem link="/orders" ><img src={avatar} alt="current orders"/></NavigationItem>
-    <NavigationItem link="/pastorders" ><img src={checkout} alt="past orders"/></NavigationItem>
-    <NavigationItem link="/faq" ><img src={help} alt="ayuda"/></NavigationItem>
-    <NavigationItem link="/contact" ><img src={contact} alt="contacto"/></NavigationItem>
+    <div className={classes.Taco}><NavigationItem link="/" ><img  src={taco} alt="build"/><span className={classes.TacoTip}>Taco</span></NavigationItem></div>
+    <div className={classes.Ordenes}><NavigationItem link="/orders" ><img src={avatar} alt="current orders"/><span className={classes.OrdenesTip}>Ordenes</span></NavigationItem></div>
+    <div className={classes.Historial}><NavigationItem link="/pastorders" ><img src={checkout} alt="past orders"/><span className={classes.HistorialTip}>Historial</span></NavigationItem></div>
+    <div className={classes.Preguntas}><NavigationItem link="/faq" ><img src={help} alt="ayuda"/></NavigationItem><span className={classes.PreguntasTip}>Preguntas</span></div>
+    <div className={classes.Contacto}><NavigationItem link="/contact" ><img src={contact} alt="contacto"/></NavigationItem><span className={classes.ContactoTip}>Contacto</span></div>
   </ul>
 )
 
